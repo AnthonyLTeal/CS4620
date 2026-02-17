cd1ef1a4d912087906006216617e63f9e9c6b544
Anthony Teal <teala@uni.edu>
2026-02-17 10:13:27 -0600

    removing debugging comments

--- Systems/PathSegmentSystems.cs
+++ Systems/PathSegmentSystems.cs
@@ -87,8 +87,8 @@ public class PathSegmentSystems
         }
         
         //Console.WriteLine();
-        Console.WriteLine("Distances: " + String.Join(", ", distances));
-        Console.WriteLine("Origin ID: " + origin.ID + " | Previous Path: " + String.Join(", ", prev));
+        //Console.WriteLine("Distances: " + String.Join(", ", distances));
+        //Console.WriteLine("Origin ID: " + origin.ID + " | Previous Path: " + String.Join(", ", prev));
         
         return new DPath(distances, prev);
     }
